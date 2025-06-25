9b16d8625c1d598b4fdbd5a5d223a784ac327cb6
Gustavo <devgustavogomes31@gmail.com>
2025-06-25 17:19:29 -0300

    first deploy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status - :response-time ms :body'));
+
 
 let persons = [
     {
@@ -91,7 +93,7 @@ app.post('/api/persons', (request, response) => {
 
     persons = persons.concat(person)
 
-    response.send(persons)
+    response.send(person)
 })
 
 const PORT = 3001
